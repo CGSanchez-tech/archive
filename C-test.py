44042a62fc1eca5c0cd915548b8d84122985788a
Carla Sanchez <72993271+CGSanchez-tech@users.noreply.github.com>
2023-11-17 19:29:06 -0500

    just code from the website

--- C-test.py
+++ C-test.py
@@ -2,22 +2,118 @@ import os
 import cv2
 import numpy as np
 from sklearn.model_selection import train_test_split
-#Used to automatically split the data intro training sets for models
+# Used to automatically split the data intro training sets for models
 from keras.utils import np_utils
-#Used for utilities related to numpy, particularly for transforming class labels to one-hot encoding.
+# Used for utilities related to numpy, particularly for transforming class labels to one-hot encoding.
 from keras.models import Sequential
-#Imports a linear stack of layers for building nueral networks layer by layer
+# Imports a linear stack of layers for building nueral networks layer by layer
 from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
+# Imports building blocks for constructing neural networks
+# Dense is a fully connected layer
+# Conv2D is a 2D convolutional layer
+# MaxPooling2D is a max pooling layer for downsampling feature maps  
+# Flatten is a layer that flattens the input, often used before connecting to fully connected layers
+# Dropout is a regularization technique. Prevents overfitting.  
 from IPython.display import display, Image
 from keras.layers import BatchNormalization
 from keras.callbacks import EarlyStopping
 
-#from keras.modelsimport Sequential: This imports the 'Sequential' class from Keras, which is a linear stack of layers for building neural networks layer by layer.
-#from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout: These import various layer types from Keras, which are building blocks for constructing neural networks.
-#Dense is a fully connected layer.
-#Conv2D is a 2D convolutional layer, commonly used for image processing tasks.
-#MaxPooling2D is a max pooling layer, used for downsampling feature maps.
-#Flatten is a layer that flattens the input, which is often used before connecting to fully connected layers.
-#Dropout is a regularization technique that randomly drops a fraction of connections during training to prevent overfitting.
+# Define the path to the dataset folders
+happy_folder = "/Users/csanchezgina/Documents/GitHub/paws-and-reflect/Happy"
+sad_folder = "/Users/csanchezgina/Documents/GitHub/paws-and-reflect/Sad"
+angry_folder = "/Users/csanchezgina/Documents/GitHub/paws-and-reflect/Angry"
 
 
+# Function to load and preprocess images
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder, filename))
+        if img is not None:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = cv2.resize(img, (48, 48))  # Resize to a fixed size for the model
+            images.append(img)
+    return images
+
+# Load images and labels for each emotion
+happy_images = load_images_from_folder(happy_folder)
+sad_images = load_images_from_folder(sad_folder)
+angry_images = load_images_from_folder(angry_folder)
+
+
+# Create labels for each emotion category
+happy_labels = [0] * len(happy_images)
+sad_labels = [1] * len(sad_images)
+angry_labels = [2] * len(angry_images)
+
+
+# Concatenate images and labels
+X = np.array(happy_images + sad_images + angry_images)
+y = np.array(happy_labels + sad_labels + angry_labels)
+
+# Normalize pixel values to range [0, 1]
+X = X.astype('float32') / 255.0
+
+# One-hot encode the labels
+y = np_utils.to_categorical(y, 3)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+#######                          ####### 
+####### Training and evalutation ####### 
+#######                          ####### 
+
+# Build the CNN model
+model = Sequential()
+
+model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(48, 48, 1)))
+model.add(BatchNormalization())
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(512, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(512, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(3, activation='softmax'))
+
+print(model.summary())
+# Compile the model with class weights
+model.compile(loss='categorical_crossentropy', optimizer="Adam", metrics=['accuracy'])
+
+####
+
+# Calculate class weights
+total_samples = len(y_train)
+class_weights = {0: total_samples / np.sum(y_train[:, 0]), 
+                 1: total_samples / np.sum(y_train[:, 1]), 
+                 2: total_samples / np.sum(y_train[:, 2])}
+# Train the model with class weights
+history = model.fit(X_train.reshape(-1, 48, 48, 1), y_train, batch_size=32, epochs=100, validation_split=0.1, class_weight=class_weights,verbose=0)
+
+# Evaluate the model on test data
+loss, accuracy = model.evaluate(X_test.reshape(-1, 48, 48, 1), y_test)
+losstr, accuracytr = model.evaluate(X_train.reshape(-1, 48, 48, 1), y_train)
+
+# Save the trained model
+model.save("facial_expression_model.h5")
+
+####
+
+print(f"Test accuracy: {accuracy*100:.2f}%")
+print(f"Train accuracy: {accuracytr*100:.2f}%")
