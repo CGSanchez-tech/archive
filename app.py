7068d11e0eef0ed694ecab93da8e005badd3df7d
Carla Sanchez <72993271+CGSanchez-tech@users.noreply.github.com>
2023-12-06 03:43:20 -0500

    notes and prompt user to enter their own photo

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ from keras.layers import Dense, Dropout, Activation, Conv2D, MaxPooling2D, Flatt
 from IPython.display import display, Image
 from keras.layers import BatchNormalization
 from keras.callbacks import EarlyStopping
+#from multiprocessing import Pool
+#new
+from keras.models import load_model
 
 print("yippie")
 
@@ -37,6 +40,35 @@ def load_images_from_folder(folder):
             images.append(img)
     return images
 
+
+#####below new: DID NOT WORK - BIG BUG: https://github.com/python/cpython/issues/98360   
+# Trying to open up the files faster to reduce run time
+# Tried to use multiprocessing to parallelize the image loading process
+# Not the best idea though, since this isn't even the part of the 
+# code that takes the most time.
+#
+#def load_image(filename):
+#    img = cv2.imread(filename)
+#    if img is not None:
+#        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#        img = cv2.resize(img, (48, 48))
+#    return img
+#
+#def load_images_from_folder_parallel(folder):
+#    images = []
+#    filenames = [os.path.join(folder, filename) for filename in os.listdir(folder)]
+#    with Pool() as pool:
+#        images = pool.map(load_image, filenames)
+#    return images
+#
+# Replace the load_images_from_folder function with load_images_from_folder_parallel
+#happy_images = load_images_from_folder_parallel(happy_folder)
+#sad_images = load_images_from_folder_parallel(sad_folder)
+#angry_images = load_images_from_folder_parallel(angry_folder)
+#
+#
+######above new
+
 # Load images and labels for each emotion
 happy_images = load_images_from_folder(happy_folder)
 sad_images = load_images_from_folder(sad_folder)
@@ -56,7 +88,7 @@ print("Labels Made")
 print("")
 end_cell1 = time.time()
 elapsed_time1 = end_cell1 - start_cell1
-print(f"Time taken for block 1: {elapsed_time1} seconds")
+print(f"Time taken for loading images: {elapsed_time1} seconds")
 print("")
 #cell2
 start_cell2 = time.time()
@@ -104,35 +136,14 @@ model.add(Dense(3, activation='softmax'))
 print("Model Summary time")
 print(model.summary())
 
-#print("######################")
-#print("happy: ", happy_labels)
-#print(len(happy_labels))
-#print("sad: ", sad_labels)
-#print(len(sad_labels))
-#print("angry: ", angry_labels)
-#print(len(angry_labels))
-#print("######################")
-
-#file_path = "./Other"  
-#file_path2 = "./Angry"
-
-#def get_file_size(file_path2):
-#    if os.path.exists(file_path2):
-#        file_size = os.path.getsize(file_path2)
-#        return file_size
-#    else:
-#        print(f"The file {file_path} does not exist.")
-#        return None
-
-#size = get_file_size(file_path2)
-#print(size)
-
 print("")
 end_cell2 = time.time()
 elapsed_time2 = end_cell2 - start_cell2
-print(f"Time taken for block 1: {elapsed_time2} seconds")
-print("")
+print(f"Time taken for CNN model: {elapsed_time2} seconds")
 
+
+print("")
+start_cell3 = time.time()
 # Compile the model with class weights
 model.compile(loss='categorical_crossentropy', optimizer="Adam", metrics=['accuracy']) 
 
@@ -155,13 +166,17 @@ losstr, accuracytr = model.evaluate(X_train.reshape(-1, 48, 48, 1), y_train)
 model.save("facial_expression_model.h5")
 print("Trained model saved")
 
-###Cell 5
-
 print(f"Test accuracy: {accuracy*100:.2f}%")
 print(f"Train accuracy: {accuracytr*100:.2f}%")
 
+end_cell3 = time.time()
+elapsed_time3 = end_cell3 - start_cell3
+print(f"Time taken for testing images : {elapsed_time3} seconds")
+print("")
+
 ###Testing on images
 
+start_cell4 = time.time()
 from keras.models import load_model
 # Load the saved model
 loaded_model = load_model("facial_expression_model.h5")
@@ -179,12 +194,29 @@ def load_images_from_folder(folder):
             images.append(img)
     return images
 
-print("Testing an image")
+#print("Testing an image")
 # Load a custom test image
 # Make this into a command in the terminal
-custom_test_image_path = "./Angry/16924834.jpg"
+#custom_test_image_path = "./Angry/02.jpg"
 
-custom_test_image = cv2.imread(custom_test_image_path)
+#custom_test_image = cv2.imread(custom_test_image_path)
+#custom_test_image = cv2.cvtColor(custom_test_image, cv2.COLOR_BGR2GRAY)
+#custom_test_image = cv2.resize(custom_test_image, (48, 48))
+#custom_test_image = custom_test_image.astype('float32') / 255.0
+
+# Reshape the image to match the model input shape
+#custom_test_image = np.expand_dims(custom_test_image, axis=0)
+#custom_test_image = np.expand_dims(custom_test_image, axis=-1)
+
+######new below
+#print("Input an image!:")
+# Load a custom test image
+# Make this into a command in the terminal
+
+custom_test_image_name = input("Enter a JPEG image: ")
+#make a format/input check maybe?
+
+custom_test_image = cv2.imread(custom_test_image_name)
 custom_test_image = cv2.cvtColor(custom_test_image, cv2.COLOR_BGR2GRAY)
 custom_test_image = cv2.resize(custom_test_image, (48, 48))
 custom_test_image = custom_test_image.astype('float32') / 255.0
@@ -192,6 +224,8 @@ custom_test_image = custom_test_image.astype('float32') / 255.0
 # Reshape the image to match the model input shape
 custom_test_image = np.expand_dims(custom_test_image, axis=0)
 custom_test_image = np.expand_dims(custom_test_image, axis=-1)
+
+########new above
 
 # Make predictions on the custom test image
 prediction = loaded_model.predict(custom_test_image)
@@ -215,9 +249,15 @@ plt.show()
 
 from PIL import Image
 # Display the original custom test image using PIL
-img_pil = Image.open(custom_test_image_path)
+img_pil = Image.open(custom_test_image_name)
 plt.imshow(np.array(img_pil))
 plt.title(f"Predicted Emotion: {predicted_emotion}")
 plt.axis('off')  # Hide axes
 plt.show()
+
+end_cell4 = time.time()
+elapsed_time4 = end_cell4 - start_cell4
+print(f"Time taken for testing images : {elapsed_time4} seconds")
+print("")
+
 
